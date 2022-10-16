35e788a8178180db19814405ab213b7f4649bebd
redskyby <pashadocenko@gmail.com>
2022-10-17 00:28:04 +0300

    add style Header and Button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
+import './Header.css';
 
 function Header(props) {
     const { onClose , user} = useTelegram();
